96f684ca03589784db41d0c6764e3a9053602106
genghaot <teogenghao@gmail.com>
2017-10-28 21:10:57 +0800

    Portfolio changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,13 @@ gulp.task('styles', function() {
 	gulp.src('sass/style.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(gulp.dest('./css/'));
+	gulp.src('bootstrap/scss/bootstrap.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./bootstrap/dist/css'));
 });
 
 //Watch task
 gulp.task('default',function() {
 	gulp.watch('sass/style.scss',['styles']);
+	gulp.watch('bootstrap/scss/bootstrap.scss',['styles']);
 });
